def84ddeeda867cf33ab86add0261f6374d366f5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-28 22:14:15 -0500

    chore(deps): bumps projen to 0.12.2

--- .projenrc.js
+++ .projenrc.js
@@ -1,39 +1,39 @@
 const { AwsCdkConstructLibrary } = require('projen');
 
 const project = new AwsCdkConstructLibrary({
-  authorAddress: "stefan.freitag@udo.edu",
-  authorName: "Stefan Freitag",
-  description:"A simple AWS budget notifier.",
-  cdkVersion: "1.76.0",
-  name: "aws_budget_notifier",
-  repository: "https://github.com/stefan.freitag/projen-budget-notifier.git",
-  catalog:{
+  authorAddress: 'stefan.freitag@udo.edu',
+  authorName: 'Stefan Freitag',
+  description: 'A simple AWS budget notifier.',
+  cdkVersion: '1.76.0',
+  name: 'aws_budget_notifier',
+  repository: 'https://github.com/stefan.freitag/projen-budget-notifier.git',
+  catalog: {
     twitter: 'stefanfreitag',
-    announce: false
+    announce: false,
   },
   codeCov: true,
   dependabot: false,
   antitamper: false,
   keywords: [
-    "aws",
-    "cdk",
-    "budget"
+    'aws',
+    'cdk',
+    'budget',
   ],
   cdkDependencies: [
-    "@aws-cdk/aws-budgets",
-    "@aws-cdk/aws-iam",
-    "@aws-cdk/aws-sns",
-    "@aws-cdk/core"
+    '@aws-cdk/aws-budgets',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-sns',
+    '@aws-cdk/core',
   ],
   java: {
-    javaPackage:"io.github.stefanfreitag.cdk.budgetnotifier",
-    mavenArtifactId: "cdkBudgetNotifier",
-    mavenGroupId: "io.github.stefanfreitag"
+    javaPackage: 'io.github.stefanfreitag.cdk.budgetnotifier',
+    mavenArtifactId: 'cdkBudgetNotifier',
+    mavenGroupId: 'io.github.stefanfreitag',
   },
-  python:{
-    module: "cdk_budget_notifier",
-    distName: "cdk-budget-notifier"
-  }
+  python: {
+    module: 'cdk_budget_notifier',
+    distName: 'cdk-budget-notifier',
+  },
 
 
 });
