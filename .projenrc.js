1fd341cafde9968755182266dcd0284e94a932b1
Stefan Freitag <stefan.freitag@rwe.com>
2023-01-06 17:17:51 +0100

    chore: upgrade to cdk v2.59.0

--- .projenrc.js
+++ .projenrc.js
@@ -7,7 +7,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'stefan.freitag@udo.edu',
   authorName: 'Stefan Freitag',
   description: 'A simple AWS budget notifier.',
-  cdkVersion: '2.42.0',
+  cdkVersion: '2.59.0',
   name: 'aws_budget_notifier',
   repository: 'https://github.com/stefanfreitag/cdk-budget-notifier.git',
   catalog: {
